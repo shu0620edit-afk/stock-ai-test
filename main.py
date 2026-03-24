de2947083e941e9acd8cf7c0cd3a06dfd38055ba
shu0620edit-afk <shu0620edit@gmail.com>
2026-03-24 22:10:53 +0900

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     api_key = os.environ.get("OPENAI_API_KEY")
     if not api_key:
         raise ValueError("OPENAI_API_KEY が設定されていません")
-
+        
     client = OpenAI(api_key=api_key)
 
     response = client.responses.create(
